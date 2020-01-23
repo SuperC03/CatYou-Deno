a00c27cc690dfc2f89c842a40ad0b1053e20a9be
Thomas <thomas.kool.12@gmail.com>
2020-01-23 20:01:54 +1300

    Remove unnecessary else blocks

--- mod.ts
+++ mod.ts
@@ -2,20 +2,17 @@ import { Cats, ICat } from './cats.ts';
 
 export const catYou = (name?: string): string => {
 	if (name) {
-        let selectedCat: ICat;
+		let selectedCat: ICat;
 		Cats.forEach((cat: ICat) => {
-            if(cat.name === name) {
-               selectedCat = cat; 
-            }
-        });
+		    if(cat.name === name) {
+		       selectedCat = cat; 
+		    }
+		});
         if(selectedCat) {
             return selectedCat.body.join('\n');
-        } else {
-            return Cats[Math.floor(Math.random() * Cats.length)].body.join("\n");
         }
-	} else {
-        return Cats[Math.floor(Math.random() * Cats.length)].body.join("\n");
-    }
+	}
+    return Cats[Math.floor(Math.random() * Cats.length)].body.join("\n");
 };
 
 
