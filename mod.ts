5d35f521283edf3cae66db776137c6d9c0625f0b
Thomas <thomas.kool.12@gmail.com>
2020-01-23 20:11:01 +1300

    Shortern catYou function to a one liner

--- mod.ts
+++ mod.ts
@@ -1,19 +1,7 @@
 import { Cats, ICat } from './cats.ts';
 
 export const catYou = (name?: string): string => {
-	if (name) {
-		let selectedCat: ICat;
-		Cats.forEach((cat: ICat) => {
-		    if(cat.name === name) {
-		       selectedCat = cat; 
-		    }
-		});
-        if(selectedCat) {
-            return selectedCat.body.join('\n');
-        }
-	}
-    return Cats[Math.floor(Math.random() * Cats.length)].body.join("\n");
+	return (Cats.find(c => c.name == name) || Cats[Math.floor(Math.random() * Cats.length)]).body.join('\n');
 };
-
 
 console.log(catYou(''));
